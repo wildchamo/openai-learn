36c22fe6174f1cef6faf9577c4bddfb4a30715d8
Jose Luis <wildchamo@gmail.com>
2025-06-15 17:54:17 -0500

    system n user

--- src/handler/chat.tsx
+++ src/handler/chat.tsx
@@ -5,11 +5,18 @@ export const handleChat = async (request: Request, env: Env) => {
 	});
 
 	const chatCompletion = await openaiClient.chat.completions.create({
-		model: 'gpt-4o-mini',
-		messages: [{ role: 'user', content: 'What is a neuron?' }],
-		max_tokens: 100,
+		model: 'gpt-4.1-nano',
+		messages: [
+			{
+				role: 'system',
+				content: 'Te llamas Wildchamo Asistant, presentate como tal! .',
+			},
+			{ role: 'user', content: 'Hola, qué eres? ' },
+		],
+		max_completion_tokens: 100,
 	});
 
 	const response = chatCompletion.choices[0].message.content;
+
 	return new Response(JSON.stringify(response), { status: 200 });
 };
