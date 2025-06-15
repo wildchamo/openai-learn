53446f1f37909d24a0f567f2b1f94afda8ab8b67
Jose Luis <wildchamo@gmail.com>
2025-06-15 17:37:10 -0500

    first use of open ai api

--- src/handler/chat.tsx
+++ src/handler/chat.tsx
@@ -1,3 +1,15 @@
+import OpenAI from 'openai';
 export const handleChat = async (request: Request, env: Env) => {
-	return new Response('Test', { status: 200 });
+	const openaiClient = new OpenAI({
+		apiKey: env.OPENAI_API_KEY,
+	});
+
+	const chatCompletion = await openaiClient.chat.completions.create({
+		model: 'gpt-4o-mini',
+		messages: [{ role: 'user', content: 'What is a neuron?' }],
+		max_tokens: 100,
+	});
+
+	const response = chatCompletion.choices[0].message.content;
+	return new Response(JSON.stringify(response), { status: 200 });
 };
